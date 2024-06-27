6e755ef9338a2bd6525b593e639eaa17fad96d16
Sauren Sharma <sharmasaurenb@gmail.com>
2024-06-27 16:52:49 +0530

    Fixed bug

--- src/app/(main)/overview/page.tsx
+++ src/app/(main)/overview/page.tsx
@@ -33,7 +33,7 @@ const Page = () => {
   return (
     <main className="flex max-md:justify-center">
       <div className="pt-10 pl-10 flex flex-wrap max-md:min-w-80 w-full">
-        {[items]?.length < 0
+        {[items]?.length > 0
           ? items?.map((shop: Shop) => {
               return <OverviewCard key={shop.id} {...shop} />;
             })
